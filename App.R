605440c20f9e162b59571d28d594f489ca5ffe4d
da5nsy <dannygarside@outlook.com>
2020-04-21 19:28:46 -0400

    added rough outline to app

--- App.R
+++ App.R
@@ -1,10 +1,33 @@
 library(shiny)
 
 ui <- fluidPage(
-  textInput("caption", "Insert DOI", "10.1080/19455224.2016.1267025"),
-  verbatimTextOutput("value")
+  titlePanel("What Am I Missing? A tool that shows the collective references and citations for a group of articles."),
+  textAreaInput("dois", "Insert DOIs", rows = 5, value = "10.1080/19455224.2016.1267025"),
+  actionButton(inputId = "go", label = "Update"),
+  textOutput("shared_references"),
+  textOutput("shared_citations")
 )
+
 server <- function(input, output) {
-  output$value <- renderText({ input$caption })
+  dois <- eventReactive(input$go, input$dois)
+  
+  # Segment dois from long list (is there a better way to do input that respects line breaks?)
+  
+  # Look up references for each doi
+  
+  # Look up citations for each doi
+  
+  # Flag any dois where reference or citation data isn't available
+  
+  # Remove dois which were in the input
+  
+  # Compute output lists
+  
+  # Present outputs
+  # multiple lines: https://stackoverflow.com/questions/23233497/outputting-multiple-lines-of-text-with-rendertext-in-r-shiny
+  
+  output$shared_references <- renderText(dois())
+  output$shared_citations <- renderText(dois())
 }
+
 shinyApp(ui, server)
